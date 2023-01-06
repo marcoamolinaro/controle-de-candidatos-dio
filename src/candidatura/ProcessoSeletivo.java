101a7539f71e73d3c0ea081d210d731e1e8594cf
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-01-06 15:14:21 -0300

    Imprementação do método selecaoCandidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,42 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("Processo Seletivo");
 		
-		analisarCandidato(1900.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(2000.0);
+//		analisarCandidato(1900.0);
+//		analisarCandidato(2200.0);
+//		analisarCandidato(2000.0);
+		
+		selecaoCandidatos();
+	}
+	static void selecaoCandidatos() {
+		
+		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "MIRELA", "DANIELA", "JORGE"};
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		
+		while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou este valor de salario " + salarioPretendido);
+			
+			if (salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+				candidatosSelecionados++;
+			}
+			candidatoAtual++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	
 	static void analisarCandidato(double salarioPretendido) {
