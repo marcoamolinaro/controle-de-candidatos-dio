20ad044ef79cc77bbadafcd03af4dc502118dc9c
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-01-06 16:00:08 -0300

    Fazendo ligações para cada candidato selecionado

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,5 +1,6 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -12,7 +13,47 @@ public class ProcessoSeletivo {
 		
 //		selecaoCandidatos();
 		
-		imprimirSelecionados();
+//		imprimirSelecionados();
+		
+		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+		for (String candidato: candidatos) {
+			entrandoEmContato(candidato);
+		}
+	}
+	
+	static void entrandoEmContato(String candidato) {
+		
+		int tentativasRealizadas = 1;
+		
+		boolean continuarTentando = true;
+		
+		boolean atendeu = false;
+		
+		do {
+			
+			atendeu = atender();
+			
+			continuarTentando = !atendeu;
+			
+			if (continuarTentando) {
+				tentativasRealizadas++;
+			} else {
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			}
+			
+		} while(continuarTentando && tentativasRealizadas < 3);
+		
+		if (atendeu) {
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+		} else {
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato 
+					+ ", NÚMERO DE TENTATIVAS " + tentativasRealizadas + " REALIZADAS");
+		}
+	}
+	
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
 	}
 	
 	static void imprimirSelecionados() {
